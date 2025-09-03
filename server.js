baa4ff4eba39d5fc72578701a62b1856f05ee1dc
Bela <imcdsls10@gmail.com>
2025-09-03 16:58:59 -0300

    Server finalizado

--- server.js
+++ server.js
@@ -1,2 +1,29 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
+const tarefasController = require('./controllers/tarefasController');
+
+const app = express();
+app.engine('handlebars', exphbs.engine({
+
+    defaultLayout: 'layout', //Define layout padrão como "layout.handlebars"
+    layoutsDir: __dirname + '/views/' //Ajusta o diretório de layouts
+
+}));
+
+app.set('view engine', 'handlebars');
+app.use(bodyParser.urlencoded({extended: true }));
+app.use(express.static('public'));
+
+//Rotas
+app.get('/', tarefasController.exibirLista);
+app.get('/tarefas/adicionar', tarefasController.exibirAdicionarTarefa);
+app.post('/tarefas', tarefasController.adicionarTarefa);
+app.get('/tarefas/:id/editar', tarefasController.exibirEdicao);
+app.post('/tarefas/:id/editar', tarefasController.editarTarefa);
+app.get('/tarefas/:id/excluir', tarefasController.excluirTarefa);
+
+//Iniciar o servidor
+app.listen(300, () => {
+    console.log("Servidor rodando em http://localhost:3000");
+});
